69f6fcee353f8fd0de4094207789ce963908d81a
Arthur Reis <arthurcereis2005@gmail.com>
2025-04-24 18:50:19 -0300

    Update Lista - 3.c

--- Lista - 3.c
+++ Lista - 3.c
@@ -1,22 +1,22 @@
-#include <stdio.h>  '
+#include <stdio.h>
 
-int main() 
+int main()  
 {
     int g;  
     float a, x;
 
     printf("Entre com o seu genero (1 para mulher, 0 para homem): ");
-    scanf("%d", &g);  
+    scanf("%d", &g);
 
     printf("Entre com sua altura: ");
-    scanf("%f", &a);  
+    scanf("%f", &a);
 
-    if (g == 1)  o
-        x = (62.1 * a) - 44.7;  
-    else
-        x = (72.7 * a) - 58;  
+    if (g == 1) 
+        x = (62.1 * a) - 44.7;
+    else 
+        x = (72.7 * a) - 58;
 
-    printf("Seu peso ideal eh: %.2f\n", x);  
+    printf("Seu peso ideal e: %.2f\n", x);
     
     return 0;  
 }

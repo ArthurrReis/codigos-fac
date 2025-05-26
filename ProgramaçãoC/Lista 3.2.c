8b6de9965b8b0a3059ab957de8b7a49558c628b8
Arthur Reis <arthurcereis2005@gmail.com>
2025-05-26 16:47:27 -0300

    Update Lista 3.2.c

--- ProgramaçãoC/Lista 3.2.c
+++ ProgramaçãoC/Lista 3.2.c
@@ -1,58 +1,49 @@
-
 #include <stdio.h>
 
-#define PI 3.14159
+int main()
+{
+	int base, altura, raio;
+	float resultado;
+	const float PI=3.14159;
+	char letra;
 
-int main() {
-    char figura;
-    float area, valor1, valor2;
-    
-    printf("CALCULADORA DE AREAS\n");
-    printf("[C]irculo\n");
-    printf("[R]etangulo\n");
-    printf("[Q]uadrado\n");
-    printf("[T]riangulo\n");
-    printf("Escolha a figura (C/R/Q/T): ");
-    scanf(" %c", &figura);
-    
-    switch(figura) {
-        case 'C':
-        case 'c':
-            printf("Digite o raio do circulo: ");
-            scanf("%f", &valor1);
-            area = PI * valor1 * valor1;
-            printf("Area do circulo: %.2f\n", area);
-            break;
-            
-        case 'R':
-        case 'r':
-            printf("Digite a base e altura do retangulo: ");
-            scanf("%f %f", &valor1, &valor2);
-            area = valor1 * valor2;
-            printf("Area do retangulo: %.2f\n", area);
-            break;
-            
-        case 'Q':
-        case 'q':
-            printf("Digite o lado do quadrado: ");
-            scanf("%f", &valor1);
-            area = valor1 * valor1;
-            printf("Area do quadrado: %.2f\n", area);
-            break;
-            
-        case 'T':
-        case 't':
-            printf("Digite a base e altura do triangulo: ");
-            scanf("%f %f", &valor1, &valor2);
-            area = (valor1 * valor2) / 2;
-            printf("Area do triangulo: %.2f\n", area);
-            break;
-            
-        default:
-            printf("Opcao invalida! Programa encerrado.\n");
-            return 1;
-    }
-    
-    return 0;
+	printf("Escolha a forma geometrica: \n(c)irculo\n(r)etangulo\n(q)uadrado\n(t)riangulo\n");
+	scanf(" %c", &letra);
+
+	switch (letra) {
+	case 'c':
+		printf("Digite o raio do circulo:");
+		scanf("%d", &raio);
+		resultado = PI *(raio*raio);
+		printf("Sua area e de %.2f", resultado);
+		break;
+	case 'r':
+		printf("Digite a base e a altura do seu retangulo:");
+		printf("Base: ");
+		scanf("%d", &base);
+		printf("Altura: ");
+		scanf("%d", &altura);
+		resultado = base*altura;
+		printf("%f", resultado);
+		break;
+	case 'q':
+		printf("Digite a medida do lado do quadrado: ");
+		scanf("%d", &base);
+		resultado = base*base;
+		printf("%f", resultado);
+		break;
+	case 't':
+		printf("Digite a base e a altura do seu triangulo:");
+		printf("Base: ");
+		scanf("%d", &base);
+		printf("Altura: ");
+		scanf("%d", &altura);
+		resultado = (base*altura)/2;
+		printf("%f", resultado);
+		break;
+	default:
+		printf("forma nao reconhecida\n");
+	}
+	return 0;
 }
 
